3a5c017165098d688bd3e5d9fa89c08ddcb33039
Gabo Esquivel <contact@gaboesquivel.com>
2021-09-18 20:14:39 -0600

    fix: endpoint url

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,7 +15,7 @@ class LoginComponent extends React.Component {
   render() {
     const { dispatch } = this.props;
         // redirect 
-    dispatch(updateGraphQLEndpoint('https://graph.blockmatic.io/v1/graphql"'));
+    dispatch(updateGraphQLEndpoint('https://graph.blockmatic.io/v1/graphql'));
     return (
       <div>
         {/* <div className="loginWrapper">
